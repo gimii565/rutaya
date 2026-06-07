6d43f278ad4ad319ced792ad06e3280e3e2a2687
gimii565 <gimii565@gmail.com>
2026-06-07 13:30:34 -0400

    feat: manifest separado para admin

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,4 +2,8 @@ from flask import Blueprint
 
 admin = Blueprint('admin', __name__)
 
+@admin.context_processor
+def inject_manifest():
+    return {'manifest_url': '/static/manifest-admin.json'}
+
 from app.admin import routes
